ff544f3b0fa4420c37cd6ca01305ea7d34b19e7f
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-13 11:47:10 +0100

    learning to use useCallback and react.memo

--- src/App.js
+++ src/App.js
@@ -1,83 +1,37 @@
-import React, { useState, useEffect } from 'react';
-
-import Post from './post';
-
-const App = ({initialCount}) => {
-
-  //let [count, setCount] = useState(initialCount); **ONLY ONE VARIABLE**
-
-  const [state,setState] = useState({
-    count: initialCount,
-    user:"Francis"
-  });
-
-  let [posts, setPosts] = useState([
-    {
-      name:'This is the first post',
-      body:'The content of the first post'
-    },
-    {
-      name:'This is the second post',
-      body:'the content of the second post'
-    }
-  ])
-
-  const subOne = () => {             
-    setState({
-      ...state,
-      count: state.count-1
-    })
-  }
-
-  const addPost = () => {
-    let newPost = {
-      name:'new post name',
-      body:'new post body'
-    }
-    setPosts([
-      ...posts,newPost])
-  }
-
-  const removePosts = () => {
-    
-    setPosts([])
-  }
+import React, { useState, useCallback } from 'react';
 
 
-  useEffect(()=>{
-    // console.log('change on state')
-  },[state])
 
-  useEffect(()=>{
-    //console.log('change on posts')
-  },[posts])
+import Title from './components/title';
+import Count from './components/count';
+import Age from './components/age';
+import CountBtn from './components/countBtn';
+import AgeBtn from './components/ageBtn';
 
-  useEffect(()=> {
-    //console.log('Mounted')
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [age, setAge] =  useState(10);
+
+
+  const incrementCount = useCallback(() => {
+    setCount(prevCount =>prevCount+1);
+  },[])
+  
+  const incrementAge = useCallback(() => {
+    setAge(prevAge =>prevAge+1);
   },[])
 
-
-  return (
+  return(
     <>
-      <h1>{state.user}</h1>
-      <h3>Count: {state.count}</h3>
-      <button onClick={ () => setState({
-          ...state,
-          count:state.count+1
-          })}>Add 1</button>
-      <button onClick={ subOne }>Subtract 1</button>
-      {/* <button onClick={()=>{setCount(initialCount)}}>Reset</button> */}
-
-      <hr/>
-      {posts.map((item, i) => {
-        return(
-          <Post item={item} key ={i} />
-        )
-      })}
-      <button onClick={addPost}>Add Post</button>
-      <button onClick={removePosts}>Remove Posts</button>
+      <Title />
+      <Count count={count}/>
+      <CountBtn handleCount={incrementCount} />
+      <Age age={age} />
+      <AgeBtn handleAge={incrementAge} />
     </>
-  );
+  )
 }
 
 export default App;
